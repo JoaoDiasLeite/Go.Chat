6d3451b5ec3e2078101ffb9131d2068117cd8652
JoaoDiasLeite <joaofilipe.diasleite@ua.pt>
2021-06-03 02:29:46 +0100

    V.1.0

---
+++ README.md
@@ -0,0 +1,3 @@
+#IFrame
+
+IFrame Test Application
---
+++ resources/model.js
@@ -0,0 +1,9 @@
+const Joi = require('joi')
+const schema = Joi.object({
+    name: Joi.string().required(),
+    username: Joi.string().required(),
+    email: Joi.string().required(),
+    password: Joi.string().required()
+
+});
+module.exports.schema = schema;
