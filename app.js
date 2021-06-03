38f10a08e8731efea70b118aacbfcaed8b19d0c2
JoaoDiasLeite <joaofilipe.diasleite@ua.pt>
2021-06-03 02:22:17 +0100

    V.1.0

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const session = require('express-session')
+const routes = require('./routes');
+const port = process.env.PORT || 8080;
+let session_key = process.env.SESSION_KEY;
+
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+
+app.use(session({
+    secret: session_key,
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use('/', routes);
+
+app.use(function(req, res) {
+    const err = "404 Route Not Found";
+
+    res.status(404).end(err);
+});
+
+app.listen(port, function() {
+    console.log(`Running on port ${port}`);
+});
+
+module.exports = app;
---
+++ resources/methods.js
@@ -0,0 +1,86 @@
+const axios = require('axios');
+
+require('dotenv').config();
+let auth_token = process.env.AUTH_TOKEN;
+let user_id = process.env.USER_ID;
+const rocketChatServer = 'http://localhost:3000';
+const rocketChatAdminAuthToken = auth_token;
+const rocketChatAdminUserId = user_id;
+
+
+
+
+
+async function fetchUser(username) {
+    const params = {
+        username: username
+    }
+
+
+    const rocketChatUser = await axios.get(`${rocketChatServer}/api/v1/users.info`, {
+        headers: {
+
+            'X-Auth-Token': rocketChatAdminAuthToken,
+            'X-User-Id': rocketChatAdminUserId
+        }
+
+    }, {
+        params: {
+            username: username
+        }
+    });
+
+    return rocketChatUser;
+}
+
+async function loginUser(username, password) {
+    const response = await axios.post(`${rocketChatServer}/api/v1/login`, {
+        username,
+        password
+    });
+
+    return response;
+}
+
+async function createUser(username, name, email, password) {
+    const rocketChatUser = await axios.post(`${rocketChatServer}/api/v1/users.create`, {
+        name,
+        email,
+        password,
+        username,
+        verified: true
+    }, {
+        headers: {
+            'X-Auth-Token': rocketChatAdminAuthToken,
+            'X-User-Id': rocketChatAdminUserId
+        }
+    })
+
+    return rocketChatUser;
+}
+
+async function createOrLoginUser(username, name, email, password, ) {
+    try {
+
+        const user = await fetchUser(username);
+
+        // Perfom login
+        return await loginUser(username, password);
+    } catch (ex) {
+        if (ex.statusCode === 400) {
+            // User does not exist, creating user
+            const user = await createUser(username, name, email, password);
+            // Perfom login
+            return await loginUser(email, password);
+        } else {
+
+            throw ex;
+        }
+    }
+}
+module.exports = {
+    createOrLoginUser,
+    fetchUser,
+    loginUser,
+    createUser
+}
