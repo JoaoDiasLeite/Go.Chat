9f4e64cda10ae196f24706e7e49649fe23f18856
JoaoDiasLeite <joaofilipe.diasleite@ua.pt>
2021-06-03 02:26:45 +0100

    Deleted file from repository only

--- resources/model.js
+++
@@ -1,9 +0,0 @@
-const Joi = require('joi')
-const schema = Joi.object({
-    name: Joi.string().required(),
-    username: Joi.string().required(),
-    email: Joi.string().required(),
-    password: Joi.string().required()
-
-});
-module.exports.schema = schema;
